288bea1f9c68443c23bec9d5030542d0d8eda799
David Roossien <david.roossien@sbcglobal.net>
2023-06-19 22:53:20 -0700

    added login JWT/paseto api support

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,25 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/davidroossien/mysimplebank/db/sqlc"
+	"github.com/davidroossien/mysimplebank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	// reduces verbosity of console output for test results
